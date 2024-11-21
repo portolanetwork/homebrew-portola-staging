2eb7c4ae11d192be7fb4fd0c42ec5934d0b13ad3
portolanetwork <sami.malik@portola.app>
2024-11-20 18:21:48 -0800

    Brew formula update for portola-staging version v0.4.21

--- Formula/staging.portola.app.rb
+++ Formula/staging.portola.app.rb
@@ -5,11 +5,11 @@
 class StagingPortolaApp < Formula
   desc ""
   homepage "https://github.com/nomad10101/portola"
-  version "0.4.14"
+  version "0.4.21"
   depends_on :macos
 
-  url "https://github.com/portolanetwork/portola-staging-release/releases/download/v0.4.14/portola-staging_Darwin_x86_64.tar.gz"
-  sha256 "a52468c5c592165ffbc3e46f1bdb7838b5c1dd99a8ab64c7534e3f8c1cebb15e"
+  url "https://github.com/portolanetwork/portola-staging-release/releases/download/v0.4.21/portola-staging_Darwin_x86_64.tar.gz"
+  sha256 "ee612a045afc096822f703d4c5be3602c0a0210e61feb5a4db71ebf7995136d7"
 
   def install
     bin.install "portd"
