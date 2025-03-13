3b0299198ca72a23f386ecad2b794e2158786191
portolanetwork <sami.malik@portolanetwork.io>
2025-03-12 21:09:41 -0700

    Brew formula update for portola-staging version v0.7.132

--- Formula/staging.portola.app.rb
+++ Formula/staging.portola.app.rb
@@ -5,12 +5,12 @@
 class StagingPortolaApp < Formula
   desc ""
   homepage "https://github.com/portolanetwork/portola"
-  version "0.7.129"
+  version "0.7.132"
   depends_on :macos
 
   if Hardware::CPU.intel?
-    url "https://github.com/portolanetwork/portola-staging-release/releases/download/v0.7.129/portola-staging_Darwin_x86_64.tar.gz"
-    sha256 "7344a0ffec84caf5b7fba5f8f7ec9b25981c145a4b1d84a3ebbdf91af6f4c285"
+    url "https://github.com/portolanetwork/portola-staging-release/releases/download/v0.7.132/portola-staging_Darwin_x86_64.tar.gz"
+    sha256 "73833d2f4cee08e5a3e5bc51350187d904c1b37f6f2b389706361cc529d16398"
 
     def install
       bin.install "staged"
@@ -20,8 +20,8 @@ class StagingPortolaApp < Formula
     end
   end
   if Hardware::CPU.arm?
-    url "https://github.com/portolanetwork/portola-staging-release/releases/download/v0.7.129/portola-staging_Darwin_arm64.tar.gz"
-    sha256 "40655ee606c5450c6ca1ad916e4a5d23a339789a894f08491d768e774e35ca9a"
+    url "https://github.com/portolanetwork/portola-staging-release/releases/download/v0.7.132/portola-staging_Darwin_arm64.tar.gz"
+    sha256 "acadc1c05a688078424dfde6686f7601bedf54a7ca0842c9ca3888a55281f180"
 
     def install
       bin.install "staged"
